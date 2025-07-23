7a09b2d29225c1940d44daaef0f403a7a98c1761
Tien Le <71688550+tientle@users.noreply.github.com>
2025-07-23 00:29:22 -0400

    run build

--- docs/_app/immutable/entry/start.CZ8GNz8P.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DHj7a4fm.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/nodes/2.Cs768MZk.js
@@ -0,0 +1,2 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/CzDqpSSO.js";import{j as e,k as o,l as s}from"../chunks/w3nCSzhC.js";var n=e(`<h1>100 Days of D3.js + data science math fundamentals</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation</p>`,1);function i(t){var a=n();s(2),o(t,a)}export{i as component};
